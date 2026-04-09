f102b50e8c8203ce76f6e16957e6127d38c8e531
Alex Mendoza <ulises.mendozah@sat.gob.mx>
2026-04-09 13:01:50 -0600

    update: sliding window exercise 2

--- typeScript/src/leetCode/arrays_and_strings/sliding_window.ts
+++ typeScript/src/leetCode/arrays_and_strings/sliding_window.ts
@@ -40,27 +40,87 @@ console.log(longestSubArray([3, 1, 2, 7, 4, 2, 1, 1, 5], 8))
  * s = "1101100111"; la repsuesta es 5, ya que si cambiamos el indice 2 se crea 1111100111.
  */
 function binaryString(s: string) {
-    //esto se puede ver como 'cuantos ceros hay en el substring, si tiene mas de un cero, ya no se se concidera'
-    let left = 0;
-    // let arrayS = Array.from(s); // se crea el array desde el string
-    let curr = 0; // como la restriccion es una suma se decalara esta variable que tendra la suma actual
+    let j = 0; //left
+    let i = 0;
+    let curr = 0; //contadores de ceros
+    let ans = 0; //longitud maxima del subarray - i
 
-    for(let right = 0; right < s.length; right++){// el tiene el indice derecho
-        //rigth debe agregar todo el tiempo a la ventana?
-
-
+    while (i < s.length) {
+        if( s[i] == "0"){ //buscamos ceros en el array
+            curr ++;
+        }
+        while( curr > 1 ) {
+            if (s[i] == '0' && s[j] == '0') {
+                i++;
+                j++;
+            }
+            j = i;
+            i = j;
+            curr = 0;
+        }
+        ans = Math.max(ans, i - j +1);
+        i++; //avance de rigth
     }
-
-    // console.log(s.slice(0,s.length).split('0'))
-    // console.log(s.slice(0,s.length).split('0').length)
-
-    // recuerda que el while es mientras no condicion sea invalida. ¿cual es la condicion invalida para que siga?
-    // la condicion invalida para que siga el while es que haya mas de un cero en el sub arrays
-    /**
-     * while subArr.count('0') > 1 
-     */
+    return ans;
 }
 
 console.log("\nbinaryString");
-binaryString("1101100111");
-// binaryString("11001100111");
+console.log(binaryString("1101100111"), "5"); // res 5
+console.log(binaryString("11001100111"), "4"); //res 4
+console.log(binaryString("110001100111"), "4"); // tres  res 4
+console.log(binaryString("011001100111"), "4"); // cero al inicio res 4
+console.log(binaryString("00110011001110"), "4"); // doble cero al inicio y final res 4
+console.log(binaryString("1101111100111"), "8"); // res 8
+console.log(binaryString("00100"), "2"); // res 2
+// binaryString("11001100111"))
+/**
+ * 1101100111
+ * i            curr = 0; i = 0; j = 0
+ * j
+ * 1
+ * 1101100111
+ *  i           curr = 0; i = 1; j = 0
+ * j
+ * 2
+ * 1101100111
+ *   i          curr = 1; i = 2; j = 0
+ * j
+ * 3
+ * 1101100111
+ *    i         curr = 1; i = 3; j = 0
+ * j
+ * 4
+ * 1101100111
+ *     i        curr = 1; i = 4; j = 0
+ * j    
+ * 5
+ * 1101100111
+ *      i       curr = 2; i = 5; j = 0; length = 5; length > ans? SI -> ans = length NO  ans = ans; ans 5
+ * j
+ * 6
+ * 1101100111
+ *      i       curr = 1; i = 6; j = 6
+ *      j   
+ * 7
+ * 1101100111
+ *       i      curr = 2; i = 7; j = 6; length = 2; length 2 > ans 5? SI -> ans = length NO  ans = ans; ans 5
+ *      j
+ * 8
+ * 1101100111
+ *       i      curr = 1; i = 8; j = 8
+ *       j
+ * 9
+ * 1101100111
+ *        i     curr = 1; i = 9; j = 8
+ *       j
+ * 10
+ * 1101100111 
+ *         i    curr = 1; i = 10; j = 8
+ *       j  
+ * 11
+ * 1101100111 while i < arr.length; 11 < 11 -> se detiene
+ *          i   curr = 1; i = 11; j = 8; length = 2; length 4 > ans 5 ? SI -> ans = length NO  ans = ans; ans 5
+ *       j     
+ * 
+ * 
+ */
