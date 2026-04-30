2e9d6fc83751e1e25a5115aac6b464c20a008be7
Alex Mendoza <ulises.mendozah@sat.gob.mx>
2026-04-30 11:19:56 -0600

    update: prefixSum example 2 numberOfWaysToSplitArray

--- typeScript/src/leetCode/arrays_and_strings/prefixSum.ts
+++ typeScript/src/leetCode/arrays_and_strings/prefixSum.ts
@@ -41,3 +41,55 @@ function answerQuerys(arr: Array<number>, querys: Array<any>, limit: number) {
 console.log("\nanswerQuerys")
 console.log(answerQuerys([1, 6, 3, 2, 7, 2], [[0, 3], [2, 5], [2, 4]],13))
 
+/**
+ * Dado un arra de numeros enteros, encuentra el numeor de formas en que se puede divir el array en subarrays tal que el la suma de los elementos del primer subarray sea mayor o igual que la segunda parte. La segunda parte debe tener al menos un numero
+ */
+
+function numberOfWaysToSplitArray(arr: Array<number>){
+
+    /**
+     * NOTAS
+     * 
+     * El array se tiene que divir en dos, por lo que al tomar un conjunto el sobrante por default es la otra mitad
+     * [10,4,-8,7] -> [10] y [4, -8, 7]; [10,4] y [-8,7]; [10,4,-8] y [7]
+     * [10,4,-8,7] -> 10 y 3;           14 y -1;            6 y 7
+     * Se debe usar el array de prefix
+     */
+
+    // Creamos el prefixSum
+    let prefix = [arr[0]]
+    for (let i = 1; i < arr.length; i++) {
+        prefix[i] = prefix[i-1] + arr[i];
+    }
+    // la idea principal de este problema es jugar con el prefixSum
+    let ans = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+        if(prefix[i-1] > (prefix[prefix.length -1] - prefix[i-1])){
+            //console.log(prefix[i-1] ,">", (prefix[prefix.length -1] - prefix[i-1]))
+            ans ++;
+        }
+        
+    }
+
+    return ans;
+
+}
+console.log("\nnumberOfWaysToSplitArray")
+console.log(numberOfWaysToSplitArray([10,4,-8,7]))
+console.log(numberOfWaysToSplitArray([10,4,12,6,4,1,-8,7]))
+
+/**
+ * NOTAS IMPORTANTES
+ * La prefixSum nos ahorra la iteracion usando un solo array con la suma acumulada de un array.
+ * Hay que tomar en cuenta esta caracteriztica de la prefixSum, ese es el verdadero uso de la prefixSum
+ * arr = [10,4,-8,7]
+ * prefixSum = [10,14,6,13]
+ * si quiero encontrar la suma de indice 1 al 3 seria 13 - 10 = 3
+ * indice 1 = 4, indice 3 = 7
+ * prefix[1] = 14, prefix[3] = 13 -> ¿Cómo llegamos al 3?
+ * lo normal seria sumar del 4 al 7 -> 4 + (-8) + 7 = 4 - 8 + 7= -4 + 7 = 3 (NORMAL - MAL USO)
+ * con prefixSum
+ * prefix[j] - prefix[i - 1] = prefix[3] - prefix[1-1] = prefix[3] - prefix[0] = 13 - 10 = 3 
+ * 
+ */
