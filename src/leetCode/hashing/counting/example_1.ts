945f38807bc69edd7e56f8243334e505c3292cba
Alex Mendoza <ulises.mendozah@sat.gob.mx>
2026-06-22 11:38:36 -0600

    update: new topic Counting in hashMap

---
+++ src/leetCode/hashing/counting/example_1.ts
@@ -0,0 +1,23 @@
+/**
+ * Example 1: You are given a string s and an integer k. Find the length of the longest substring that contains at most k distinct characters.
+
+For example, given s = "eceba" and k = 2, return 3. The longest substring with at most 2 distinct characters is "ece".
+ */
+
+function findLongestSubstring(s: string, k: number): number {
+
+
+
+
+    let ans = 0;
+
+    return ans;
+}
+
+/**
+ * NOTAS
+ * 
+ * -    Como es un substring se pude usar sliding window (se debe).
+ * -    La descripcion de este ejemplo menciona que se podria resolver el problema si no tuviera que considerar varias letras
+ * -    Esta es la ventaja del hashMap, nos puede ayudar en este problema al considear varias letras y su numero de aparciones.
+ */
