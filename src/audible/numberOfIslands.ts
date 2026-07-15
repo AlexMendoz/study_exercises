24a8928d59d85def78ff16d6dfcff67989ef58aa
Alex Mendoza <ulises.mendozah@sat.gob.mx>
2026-07-15 17:37:09 -0600

    new: Number of Islands, only text problem

---
+++ src/audible/numberOfIslands.ts
@@ -0,0 +1,54 @@
+/**
+ * 200. Number of Islands
+Medium
+Topics
+premium lock icon
+Companies
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ 
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+ */
+
+function numberOfIslands(grid: number[][]): number {
+    let ans: number = 0;
+
+
+
+    return ans;
+}
+
+/**
+ * NOTAS    
+ * Este ejercicio ya lo hice con arturo pero no lo logre replicar del todo pero ya entiendo mas como va el problema
+ * -    Recuerda que que hay un eje para ver las islas
+ * -    No es necesario que se guarden cosas porque podemos ver los 1 cuando vamos recorriendo el array
+ */
