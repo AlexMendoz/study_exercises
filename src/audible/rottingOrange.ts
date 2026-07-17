78f0dd0d3a7ad448e8d42693253b91b1091b9a1b
Alejandro Mendoza <alexzhk@MacBook-Air.local>
2026-07-17 08:35:51 -0600

    update: some comments for recursivity

--- src/audible/rottingOrange.ts
+++ src/audible/rottingOrange.ts
@@ -41,13 +41,62 @@ function isValid(row:number, col: number, grid: number[][]):boolean{
     return (row >= 0 && row < grid.length && col >= 0 && col < grid[0].length);
 }
 
+//funcion para determinar si una naranja esta podrida
+function contagio(row: number, col: number, grid: number[][]): number {
+    //antes de hacer cualquier operacion se tiene que validar que cumpla las condiciones del problema
+    // revisar que este dentro de los limites
+    if (!isValid(row, col, grid)) {
+        return 0;
+    }
+    // solo me interesa hacer algo si mi valor es 2
+    if (grid[row][col] != 2) {
+        return 0;
+    }
+
+    let ans = 0;
+    //recuerda: arriba, derecha, abajo, izquierda
+    const directions = [[-1,0], [0,1], [1,0], [0,-1]];
+    for (const direction of directions) {
+        //aqui es donde voy a revisar si mi elemento adyacente es 1 o 0
+        // si es 1, lo combierto en 2; si es 0 no hago nada
+        const newRow = row + direction[0];
+        const newCol = col + direction[1];
+        //se tienen que validar los nuevos limites, por eso en el sigueinte if salia undefined
+        if (!isValid(newRow, newCol, grid)) {
+            continue;
+        }
+        //aqui voy a ver si mi valor adyacente es 1, si es 1, lo cambio a 2
+        if (grid[newRow][newCol] == 1) {
+            grid[newRow][newCol] = 2;
+            ans++;
+            contagio(newRow, newCol, grid)
+            console.log(grid)
+            
+        }
+    }
+
+    return ans;
+
+}
+
 function rottingOranges(grid: number[][]): number{
     let minutes: number = 0;
-
-
-
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            minutes = minutes + contagio(i,j,grid);
+            // console.log(grid)
+        }
+        
+    }
+    console.log(grid)
     return minutes;
 }
+
+console.log(rottingOranges([
+    [2,1,1],
+    [1,1,0],
+    [0,1,1]]
+))
 
 
 /**
@@ -57,5 +106,8 @@ function rottingOranges(grid: number[][]): number{
  * - Se usa parte del codigo del ejercicio anterior por que tenemos que tener direcciones a donde mirar
  * - Tambien se usa una funcion para validar si estamos dentro de los limites de la matriz
  * - Se tienen tres casos, 2 para una naranja podrida (contagia), 1 para una naranja y 0 para un ligar vacio, la funcion tiene que veriricar si esta en un 2 y si el adyacente es 1 o 0 
+ *      - Si es 2: tengo que revisar mis adyacentes, si es 1 la contagio, si es 0 no hago nada
+ *      - Si es 1: no hago nada
+ *      - Si es 0: no hago nada
  * 
  */
