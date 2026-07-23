75bdd29021cb3cbb7dae4f845864576e87ad7f1a
Alejandro Mendoza <alexzhk@MacBook-Air.local>
2026-07-23 12:45:10 -0600

    new: meetingsRooms 2 completed and notes

---
+++ src/audible/kClosetsPointsToOrigin/kClosetsPointsToOrigin2.ts
@@ -0,0 +1,77 @@
+/**
+ * 973. K Closest Points to Origin
+    Medium
+
+    Topics
+    premium lock icon
+    Companies
+    Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+
+    The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+
+    You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+
+    Example 1:
+
+    Input: points = [[1,3],[-2,2]], k = 1
+    Output: [[-2,2]]
+    Explanation:
+    The distance between (1, 3) and the origin is sqrt(10).
+    The distance between (-2, 2) and the origin is sqrt(8).
+    Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+    We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].
+    Example 2:
+
+    Input: points = [[3,3],[5,-1],[-2,4]], k = 2
+    Output: [[3,3],[-2,4]]
+    Explanation: The answer [[-2,4],[3,3]] would also be accepted.
+
+    Constraints:
+
+    1 <= k <= points.length <= 104
+    -104 <= xi, yi <= 104
+ */
+
+function distance2(x1: number,y1: number): number {
+    //al comparar con el origen, no necesito x2 y y2
+    return (Math.sqrt(Math.pow((x1),2) +  Math.pow((y1),2)));
+}
+
+function kClosest2(points: number[][], k: number): number[][]{
+    
+    const sortPoints = [...points].sort( // aqui se crea la copia de los puntos, con sort, voy a ordenar con base a mi fucnion distancia
+        (point1,point2) => { //cada point es [x,y], por eso en la funcion se le pasa el valor de x y y
+             return (distance2(point1[0],point1[1]) - distance2(point2[0], point2[1]))
+        }
+    )
+    return sortPoints.slice(0,k);
+}
+
+console.log(kClosest2([[1,3],[-2,2]], 1))
+
+console.log(kClosest2([[3,3],[5,-1],[-2,4]], 2))
+
+/**
+ * Para este ejercicio es importante entender como funciona el metodo sort.
+ * Metodo sort: Este metodo recibe una funcion de comparacion (a,b) => { forma de hacer la comparacion a - b}
+ *  a - b: es negativo, a va antes
+ *  a - b: es positivo, b va antes
+ *  a - b: es cero, son equivalente para el orden
+* const sortPoints = [...points].sort( // aqui se crea la copia de los puntos, con sort, voy a ordenar con base a mi fucnion distancia
+            (point1,point2) => { //cada point es [x,y], por eso en la funcion se le pasa el valor de x y y
+                return (distance2(point1[0],point1[1]) - distance2(point2[0], point2[1]))
+            }
+        )
+ * Sort recibe (point1, point2), que son dos puntos de esta forma [x,y] point1 = a, point2 = b
+ * y la forma de comparar es haciendo la evaluacion con la funcion de la distancia
+ *      distance2(point1[0],point1[1]) - distance2(point2[0], point2[1])
+ * Entonces, los puntos [x,y] se van intercambiando de lugar
+ * 
+ */
+/**
+ * NOTAS
+ * 
+ * A simple vista parece un problema de una funcion que recibe un vector de elementos y se tienen que evaluar, se hace un operacion por cada punto, se detemrina cual o cuales cumplen con distacion mas corta al origen y regresalos.
+ * 
+ * Para lo formula de la distancia, es para calcular la distancia entre dos puntos, pero el problema solo me un punto, ¿Cúal es el otro punto?, el origen (0,0)
+ */
---
+++ src/audible/meetingRooms/meetingsRooms.ts
@@ -0,0 +1,76 @@
+/**
+ * Meeting Rooms II
+Medium
+Topics
+Company Tags
+
+Hints
+Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), find the minimum number of rooms required to schedule all meetings without any conflicts.
+
+Note: (0,8),(8,10) is NOT considered a conflict at 8.
+
+Example 1:
+
+Input: intervals = [(0,40),(5,10),(15,20)]
+
+Output: 2
+Explanation:
+room1: (0,40)
+room2: (5,10),(15,20)
+
+Example 2:
+
+Input: intervals = [(4,9)]
+
+Output: 1
+Constraints:
+
+0 <= intervals.length <= 500
+0 <= intervals[i].start < intervals[i].end <= 1,000,000
+ */
+type Intervals = [number,number]
+
+function viewLimits(final: number, intervals: Intervals[]) {
+    let roomCount = 0;
+    while(intervals.length > 0){
+        // console.log("funcion")
+        for (const [init, final] of intervals) {
+            console.log(final, "<", init);
+            //validacion por si no pasa lo esperado
+            if (!(final < init)) {
+                break;
+            }
+            if ((final < init)) {
+                let newFinal = init;
+                intervals = intervals.slice(1);
+                
+                viewLimits(newFinal,intervals);
+            }
+        }
+        roomCount++;
+    }
+
+    console.log(roomCount);
+}
+
+
+console.log(minMeetingRooms([[0,40],[5,10],[15,20]]))
+
+
+/**
+ * 
+ * NOTAS
+ * 
+ * El problema me pide encontrar la cantidad de salas necesarias para que las reuniones se tengan sin ningun conflicto,
+ * Me dan un array de horarios [(0,40),(5,10),(15,20)] y de aqui puedo deducir que se necesitan dos salas, una para la reunion de (0,40) y otra sala para las reuniones de (5,10) y (15,20) ya que no se empalman
+ * 
+ * 
+ * 
+ * IDEAS
+ * 
+ * Como primer idea, se puede hacer una funcion. para comparar los intervalos, verificamos cuales son los valores extremos, el mas pequeño y el mas grande.
+ * Con estos limites, voy comparando los valores de cada reunion,
+ * 
+ * 
+ * Tomamos un horario como pivote y lo vamos comparando con los demas, por ejemplo, tomamos (0,40) y evaluamos con los otros limites (5,10) y (15,20), 40 < 5 ? no, siguiente, 40 < 15 ? no, ya terminamos de comparar todos los valores, entonces quitamos (0,40) y aumentamos el contador de salas, ahora tomamoms el (5,10) y validamos 10 < 15 ? si, hemos terminado, y aumentamos el contador de salas
+ */
